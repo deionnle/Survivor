4fdfdd8ca7d4ee8199095d278bb2b2cd4f7f590e
deionnle <render48@yandex.ru>
2024-07-02 08:22:26 +0300

    add Squirrel.java

---
+++ Squirrel.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Level1 {
+    public static int squirrel (int N) {
+        int factorial = 1;
+        for (int i = 2; i <= N; i ++) {
+            factorial *= i;
+        }
+        while (factorial > 10) {
+            factorial /= 10;
+        }
+        return factorial;
+    }
+}
