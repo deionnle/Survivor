2568d85b6b62c8eb54296f446ae337fbb28f2f4a
deionnle <render48@yandex.ru>
2024-07-18 10:35:28 +0300

    add undoTest

--- BastShoeTest.java
+++ BastShoeTest.java
@@ -111,4 +111,61 @@ public class BastShoeTest {
         command = "3 0";
         assertEquals("П", BastShoe.BastShoe(command));
     }
+
+    @Test
+    public void undoTest() {
+
+        String command = "1 Привет";
+        assertEquals("Привет", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("", BastShoe.BastShoe(command));
+        command = "1 Привет";
+        assertEquals("Привет", BastShoe.BastShoe(command));
+        command = "1 , Мир!";
+        assertEquals("Привет, Мир!", BastShoe.BastShoe(command));
+        command = "1 ++";
+        assertEquals("Привет, Мир!++", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("Привет, Мир!", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("Привет", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("Привет", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("Привет", BastShoe.BastShoe(command));
+        command = "5";
+        assertEquals("Привет, Мир!", BastShoe.BastShoe(command));
+        command = "5";
+        assertEquals("Привет, Мир!++", BastShoe.BastShoe(command));
+        command = "5";
+        assertEquals("Привет, Мир!++", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("Привет, Мир!", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("Привет", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("Привет", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("Привет", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("Привет", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("Привет", BastShoe.BastShoe(command));
+        command = "1 , Мир!";
+        assertEquals("Привет, Мир!", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("Привет", BastShoe.BastShoe(command));
+        command = "4";
+        assertEquals("Привет", BastShoe.BastShoe(command));
+        command = "5";
+        assertEquals("Привет, Мир!", BastShoe.BastShoe(command));
+        command = "5";
+        assertEquals("Привет, Мир!", BastShoe.BastShoe(command));
+    }
 }
