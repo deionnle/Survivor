5511115b4a6fc5e77935f057d98ba3cb00b6bb39
deionnle <render48@yandex.ru>
2024-07-19 03:37:49 +0300

    add HogwartsTest.java

---
+++ HogwartsTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HogwartsTest {
+
+    @Test
+    public void referenceTest() {
+        String input = "вибк";
+        assertEquals("викб", Hogwarts.BiggerGreater(input));
+    }
+
+    @Test
+    public void reference2Test() {
+        String input = "вкиб";
+        assertEquals("ибвк", Hogwarts.BiggerGreater(input));
+    }
+
+    @Test
+    public void reference3Test() {
+        String input = "нклм";
+        assertEquals("нкмл", Hogwarts.BiggerGreater(input));
+    }
+
+    @Test
+    public void reference4Test() {
+        String input = "fff";
+        assertEquals("", Hogwarts.BiggerGreater(input));
+    }
+
+    @Test
+    public void myTest() {
+        String input = "оса";
+        assertEquals("сао", Hogwarts.BiggerGreater(input));
+    }
+    @Test
+    public void my2Test() {
+        String input = "абвгдеж";
+        assertEquals("абвгдже", Hogwarts.BiggerGreater(input));
+    }
+
+    @Test
+    public void oaoTest() {
+        String input = "оао";
+        assertEquals("ооа", Hogwarts.BiggerGreater(input));
+    }
+
+    @Test
+    public void oaаoTest() {
+        String input = "оаао";
+        assertEquals("оаоа", Hogwarts.BiggerGreater(input));
+    }
+
+    @Test
+    public void abcTest() {
+        String input = "aabbcde";
+        assertEquals("aabbced", Hogwarts.BiggerGreater(input));
+    }
+}
