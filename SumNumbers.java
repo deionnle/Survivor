b0548047be80f11e2812a034bbc1fa82dbc69939
deionnle <render48@yandex.ru>
2024-12-20 19:27:52 +0300

    added changes 2 (ClearCode)

--- SumNumbers.java
+++ SumNumbers.java
@@ -1,16 +1,19 @@
 import java.util.*;
 
 public class SumNumbers {
-    public static int SumOfThe(int N, int [] data) {
-        int res = 0;
+    public static int SumOfThe(int N, int[] data) {
         int total = Arrays.stream(data).sum();
-        for (int i = 0; i < data.length; i ++) {
+        return findMatchingElement(data, total);
+    }
+
+
+    private static int findMatchingElement(int[] data, int total) {
+        for (int i = 0; i < data.length; i++) {
             if (data[i] == total - data[i]) {
-                res = data[i];
-                break;
+                return data[i];
             }
         }
-        return res;
+        return 0;
     }
 }
 
